f41049bfae7365403fc805aabf428a00f6b01535
santyyoshi <santyyoshi@gmail.com>
2024-12-09 21:52:47 -0500

    Room

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
+import RoomSeparate from "./pages/room-separate/room-separate";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/rooms/:idhotel" element={<RoomSeparate/>}/> {/* Falta ruta de habitaciones del hotel */}
+        <Route path="/room-separate/:id" element={<RoomSeparate/>}/>
       </Routes>
     </BrowserRouter>
   );
