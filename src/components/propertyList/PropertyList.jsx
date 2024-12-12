11ba4d11d2e7b0eceea27597a7843ef837e72d77
sergioDavidCano <sergio0201david@gmail.com>
2024-12-11 20:29:59 -0500

    a

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -3,15 +3,7 @@ import "./propertyList.css";
 
 const PropertyList = () => {
   const { data, loading, error } = useFetch("/hotels/countByType");
-<<<<<<< HEAD
-
-  const images = [
-    "https://ebents.com/incubandoexitos/wp-content/uploads/2018/02/elegir-el-lugar-ideal-negocio.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdDmH8pxxmlEPgKBGY4q-JuDzCXmy6MGTlTw&s",
-
-  ];
-=======
->>>>>>> 47cc3b354b737b983352de487760a7e602402269
+  
   return (
     <div className="pList">
       {loading ? (
