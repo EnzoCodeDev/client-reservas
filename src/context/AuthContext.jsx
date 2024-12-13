2df9895c1e64f5c5a91c910a5b0f10ea590183b7
Emilio Lopez <andreewjoob@gmail.com>
2024-12-12 22:23:54 -0500

    .

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,16 @@
 import { createContext, useEffect, useReducer } from "react";
 
+// Estado inicial, recuperando el usuario desde el localStorage si existe
 const INITIAL_STATE = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
 };
 
+// Creamos el contexto con el estado inicial
 export const AuthContext = createContext(INITIAL_STATE);
 
+// Reducer para manejar las acciones de login y logout
 const AuthReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN_START":
@@ -39,21 +42,26 @@ const AuthReducer = (state, action) => {
   }
 };
 
+// Componente que envuelve toda la aplicación y proporciona el estado y los métodos a través del contexto
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
+  // Efecto para sincronizar el estado de usuario con el localStorage
   useEffect(() => {
     if (state.user) {
+      // Si hay usuario, lo guardamos en localStorage
       localStorage.setItem("user", JSON.stringify(state.user));
     } else {
+      // Si no hay usuario, eliminamos los datos del localStorage
       localStorage.removeItem("user");
     }
   }, [state.user]);
 
+  // Función para hacer logout y borrar los datos del usuario
   const logout = () => {
     dispatch({ type: "LOGOUT" });
   };
-  
+
   return (
     <AuthContext.Provider
       value={{
@@ -61,8 +69,7 @@ export const AuthContextProvider = ({ children }) => {
         loading: state.loading,
         error: state.error,
         dispatch,
-        logout, 
-        
+        logout,
       }}
     >
       {children}
